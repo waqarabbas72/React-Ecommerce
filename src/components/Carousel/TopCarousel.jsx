b187f65509bbc00f4c5b40ee21d8213778964121
waqarabbas72 <sheraliate751@gmail.com>
2023-09-04 19:24:28 +0500

    carousel bg

--- src/components/Carousel/TopCarousel.jsx
+++ src/components/Carousel/TopCarousel.jsx
@@ -23,7 +23,7 @@ const TopCarousel = () => {
             <div className='w-full h-[50vh] md:h-[80vh]  flex  items-center justify-center  bg-gray-100' style={{
                 background: `url(${slide1}) center/cover no-repeat`
             }}>
-                <div className='flex flex-col md:flex-row  w-[80%] items-start md:items-center'>
+                <div className='bg-gray-800 bg-opacity-50 flex flex-col md:flex-row  w-full h-full p-10 items-start md:items-center'>
                     <div className=' flex-1 pt-10 md:pt-0 md:ml-5 text-start'>
                         <p className='text-xl text-white font-bold'>New Arrivals 2023</p>
                         <h1 className='text-5xl sm:text-6xl lg:text-7xl xl:text-8xl  my-5 text-white'>The Clothing <br /> Collection</h1>
@@ -34,7 +34,7 @@ const TopCarousel = () => {
             <div className='w-full h-[50vh] md:h-[80vh]  flex  items-center justify-center  bg-gray-100' style={{
                 background: `url(${slide2}) center/cover no-repeat`
             }}>
-                <div className='flex flex-col md:flex-row  w-[80%] items-start md:items-center'>
+                <div className='bg-gray-800 bg-opacity-50 flex flex-col md:flex-row  w-full h-full p-10 items-start md:items-center'>
                     <div className=' flex-1 pt-10 md:pt-0 md:ml-5 text-start'>
                         <p className='text-xl text-white font-bold'>New Arrivals 2023</p>
                         <h1 className='text-5xl sm:text-6xl lg:text-7xl xl:text-8xl  my-5 text-white'>Best Electronics <br /> Collection</h1>
@@ -45,7 +45,7 @@ const TopCarousel = () => {
             <div className='w-full h-[50vh] md:h-[80vh]  flex  items-center justify-center  bg-gray-100' style={{
                 background: `url(${slide3}) center/cover no-repeat`
             }}>
-                <div className='flex flex-col md:flex-row  w-[80%] items-start md:items-center'>
+                <div className='bg-gray-800 bg-opacity-50 flex flex-col md:flex-row  w-full h-full p-10 items-start md:items-center'>
                     <div className=' flex-1 pt-10 md:pt-0 md:ml-5 text-start'>
                         <p className='text-xl text-white font-bold'>New Arrivals 2023</p>
                         <h1 className='text-5xl sm:text-6xl lg:text-7xl xl:text-8xl  my-5 text-white'>Our Jewellery <br /> Collection</h1>
