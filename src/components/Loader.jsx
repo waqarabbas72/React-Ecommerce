0770b65ed6637a57040e3e54fc0bc725b51bebd1
Waqar Abbas <waqarabas72@gmail.com>
2023-12-18 15:24:10 +0500

    changes made

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -9,11 +9,11 @@ function index() {
             <div className="w-8 h-8 bg-indigo-700 rounded-full" />
             <div className="w-8 h-8 bg-indigo-700 rounded-full" />
           </div>
-          <div className="flex gap-10 justify-center mt-10">
+          {/* <div className="flex gap-10 justify-center mt-10">
             <div className="w-8 h-8 bg-indigo-200 rounded-full animate-bounce" />
             <div className="w-8 h-8 bg-indigo-200 rounded-full animate-bounce" />
             <div className="w-8 h-8 bg-indigo-200 rounded-full animate-bounce" />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
