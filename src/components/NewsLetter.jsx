7b56184d11a88e3f27deb0b9f58e4419022b5f3d
Waqar Abbas <waqarabas72@gmail.com>
2024-01-09 19:21:13 +0500

    change

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -6,6 +6,7 @@ const NewsLetter = () => {
             <div className="text-center mt-10 lg:mt-24 mb-7">
                 <p className="text-rose-800 text-2xl sm:text-5xl">Subscribe to our Newsletter</p>
             </div>
+
             <div className=" w-full  relative flex items-center justify-center">
                 <img src="https://i.ibb.co/4sYZ8gC/img-2.png" alt="dining" className="w-full h-full absolute z-0 hidden xl:block" />
                 <img src="https://i.ibb.co/bbS3J9C/pexels-max-vakhtbovych-6301182-1.png" alt="dining" className="w-full h-full absolute z-0 hidden sm:block xl:hidden" />
@@ -13,7 +14,7 @@ const NewsLetter = () => {
                 <div className="bg-gray-800 bg-opacity-80 md:my-16 lg:py-16 py-5 w-full md:mx-24 md:px-12 px-4 flex flex-col items-center justify-center relative z-10">
                     <h1 className="text-2xl sm:text-4xl font-semibold text-white text-center">Don’t miss out!</h1>
                     <p className="text-xs leading-normal text-center text-white sm:text-sm mt-3 sm:mt-6">
-                        Subscribe to your newsletter to stay in the loop. Our newsletter is sent once in a week on every friday so subscribe to get latest news and updates.
+                        Subscribe to your newsletter to stay in the looP. Our newsletter is sent once in a week on every friday so subscribe to get latest news and updates.
                     </p>
                     <div className="sm:border border-white flex-col sm:flex-row  flex items-center lg:w-5/12 w-full mt-5 sm:mt-12 space-y-4 sm:space-y-0">
                         <input className="border border-white sm:border-transparent text-base w-full font-medium leading-none text-white p-2 sm:p-4 focus:outline-none bg-transparent placeholder-white" placeholder="Email Address" />
